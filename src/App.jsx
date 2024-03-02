6de084b2043400bcf4aad90813366be1a2d255d8
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-03-02 12:58:41 +0530

    button added for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
-import {BrowserRouter, Route,Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Landing from "./components/Landing";
 function App() {
-
   return (
     <div>
-    <div style={{backgroundColor:"black",color:"white"}}>
-     hi this is a topbar!
+      <div>
+        <button
+          onClick={() => {
+            window.location.href = "/";
+          }}
+        >
+          Landing Page 
+        </button>
+        <button
+          onClick={() => {
+            window.location.href = "/dashboard";
+          }}
+        >
+          Dashboard
+        </button>
+      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-    <BrowserRouter>
-    <Routes>
-    <Route path="/dashboard" element={<Dashboard/>}/>
-    <Route path="/" element={<Landing/>}/>
-    </Routes>
-    </BrowserRouter>
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
