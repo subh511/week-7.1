00bc18e4c86ca25a80219b81dfff27116368c96d
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-03-02 12:41:38 +0530

    browserRouter method

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,17 @@ import Landing from "./components/Landing";
 function App() {
 
   return (
+    <div>
+    <div style={{backgroundColor:"black",color:"white"}}>
+     hi this is a topbar!
+    </div>
     <BrowserRouter>
     <Routes>
     <Route path="/dashboard" element={<Dashboard/>}/>
     <Route path="/" element={<Landing/>}/>
     </Routes>
     </BrowserRouter>
+    </div>
   )
 }
 
